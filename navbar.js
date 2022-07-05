475690e68311e9664442f83e9a8ffbe681e57430
MazenAsfour <108189983+MazenAsfour@users.noreply.github.com>
2022-07-05 16:55:10 +0300

    Add files via upload

---
+++ navbar.js
@@ -0,0 +1,16 @@
+const links=document.querySelector(".links");
+const nav=document.querySelector("nav");
+let number=0;
+console.log(links);
+console.log(nav.style.width);
+
+function toggle1(){
+
+    if(links.style.display==="block"){
+        links.style.display="none";
+    }
+    else{
+        links.style.display="block";
+        links.style.display="block";
+    }
+}
